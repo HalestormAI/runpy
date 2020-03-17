0d1d17c0c0e63fcc2f534c5596c5c6adcde339f3
Ian Hales <ian@reachrobotics.com>
2020-03-17 00:06:26 +0000

    Moved activity_server.py back to the backend root. Added CORS support.

--- backend/activity_server.py
+++ backend/activity_server.py
@@ -1,9 +1,9 @@
 import logging
 import sys
 
-from flask import Flask
-
 from core.config import Config
+from flask import Flask
+from flask_cors import CORS
 from resources import activity_search, activity_dl, frontend
 from running_stats import register_handlers
 
@@ -22,6 +22,8 @@ def setup_logger():
 
 def create_app(config_filename):
     app = Flask(__name__)
+    cors = CORS(app)
+    app.config['CORS_HEADERS'] = 'Content-Type'
     app.config.from_object(config_filename)
 
     activity_search.blueprint(app)
