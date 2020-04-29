045dada1be29bd2c95eb7960a53d57cd048dd075
Ian Hales <contact@ian-hales.com>
2020-04-29 17:52:32 +0100

    Frontend: Add Leaflet React plugins for Bing maps

--- frontend/src/components/ui/pages/MapVis/index.js
+++ frontend/src/components/ui/pages/MapVis/index.js
@@ -1,20 +1,32 @@
 import React from "react";
-import {Map, TileLayer, Marker, Popup} from "react-leaflet"
+import {LayersControl, Map, Marker, Popup, TileLayer} from "react-leaflet"
+import {BingLayer} from "react-leaflet-bing-v2"
+import config from "../../../../app/config/config";
+
+const {BaseLayer} = LayersControl;
 
 export default function MapVisPage(props) {
     const [state, setState] = React.useState({
-        lat: 51.478060,
-        lng: -2.627330,
-        zoom: 13
+        lat: 51.49686,
+        lng: -2.54510,
+        zoom: 15
     });
 
     const position = [state.lat, state.lng];
     return (
         <Map center={position} zoom={state.zoom}>
-            <TileLayer
-                attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-                url='https://{s}.tile.osm.org/{z}/{x}/{y}.png'
-            />
+            <LayersControl>
+                <BaseLayer checked name='Ordnance Survey'>
+                    <BingLayer bingkey={config.api_keys.bing_maps} type="OrdnanceSurvey"/>
+                </BaseLayer>
+                <BaseLayer name='OSM'>
+                    <TileLayer
+                        attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                    />
+                </BaseLayer>
+
+            </LayersControl>
             <Marker position={position}>
                 <Popup>
                     A pretty CSS3 popup. <br/> Easily customizable.
