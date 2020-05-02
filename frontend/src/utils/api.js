9ba2632f7d8342583106ea1ae7c40c32467bb1ec
Ian Hales <contact@ian-hales.com>
2020-05-03 00:46:33 +0100

    Make sure the api always notifies completion even under failure

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -4,8 +4,6 @@ async function call(dispatch, url_suffix, done_cb, success_cb, error_cb) {
     const url = config.api_root + url_suffix;
     try {
         const rawResponse = await fetch(url);
-        dispatch(done_cb());
-
         const response = await rawResponse.json();
 
         if (response["status"] === "error") {
@@ -19,6 +17,8 @@ async function call(dispatch, url_suffix, done_cb, success_cb, error_cb) {
         dispatch(error_cb({
             message: error.toString()
         }));
+    } finally {
+        dispatch(done_cb());
     }
 }
 
