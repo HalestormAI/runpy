3efb04a9e424608a455f6d5541f741fb71b1e881
Ian Hales <contact@ian-hales.com>
2020-05-04 22:45:30 +0100

    Frontend: Moved randomString generator to test-utils script

--- frontend/src/utils/__tests__/plot.test.js
+++ frontend/src/utils/__tests__/plot.test.js
@@ -1,11 +1,5 @@
 import {defaultPlotLayout, initialPlotState} from "../plot";
-
-const randomString = () => Math.random()
-    .toString(36)
-    .substring(2, 15) + Math.random()
-    .toString(36)
-    .substring(2, 15);
-
+import {randomString} from "../../test-utils";
 
 describe("Helper functions for plotting from the plot module", () => {
 
